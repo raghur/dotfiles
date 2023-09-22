044fc29d0464aedf17fef931cae59ba3c0e1a15a
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2023-09-22 10:05:03 +0530

    fix nvr remote address

--- dot_zsh.d/custom.zsh
+++ dot_zsh.d/custom.zsh
@@ -76,7 +76,7 @@ alias aptup='sudo apt update'
 alias aptug='sudo apt full-upgrade'
 alias myip="curl ifconfig.co"
 alias vim=nvim
-export NVIM_LISTEN_ADDRESS=/var/tmp/nvim.$USER.sock
+export NVIM_LISTEN_ADDRESS="$XDG_RUNTIME_DIR/nvim.sock"
 alias xnvr=nvr_xdo
 alias proot='pushd $(git rev-parse --show-toplevel)'
 setopt correct
