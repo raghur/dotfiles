7d4c027b685c70ff5c9506840a9844f64eaccde8
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2024-01-16 13:16:22 +0530

    shot.sh: flameshot with OCR script

---
+++ bin/executable_shot.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+flameshot gui --raw -p /tmp/last-screenshot.png \
+    | convert -resize 400% png:- png:- \
+    | tesseract stdin stdout -l eng --psm 6 \
+    | xclip -in -selection clipboard
