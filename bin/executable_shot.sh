5e1dbad51cc275c2e34bdee16eb84d8d274054ec
Raghu Rajagopalan <raghu.rajagopalan@rockwellautomation.com>
2024-12-31 16:38:00 +0530

    misc changes: shot.sh, git rerere

--- bin/executable_shot.sh
+++ bin/executable_shot.sh
@@ -1,6 +1,5 @@
 #! /bin/sh
-flameshot gui --raw -p /tmp/last-screenshot.png \
+flameshot gui --raw \
     | convert -resize 400% png:- png:- \
     | tesseract stdin stdout -l eng --psm 6 \
     | xclip -in -selection clipboard
-
