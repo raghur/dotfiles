1245bb9e1bb18ce2d778991c2ea1846e1e099e1d
raghur <raghu.rajagopalan@gmial.com>
2024-12-31 13:49:25 +0530

    fish shell

---
+++ private_dot_config/private_fish/conf.d/systemd-abbr.fish
@@ -0,0 +1,4 @@
+abbr -a -g sc systemctl
+abbr -a -g scu systemctl --user
+abbr -a -g jr journalctl
+abbr -a -g jru journalctl --user
---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,3 @@
+if status is-interactive
+    starship init fish | source   # Commands to run in interactive sessions can go here
+end
