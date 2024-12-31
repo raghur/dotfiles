db16ac0f78f61133b48fd2239473545ab462b479
Raghu Rajagopalan <raghu.rajagopalan@rockwellautomation.com>
2024-12-31 16:45:39 +0530

    tmux: select-nvim improvements

--- dot_tmux/executable_select-nvim.sh
+++ dot_tmux/executable_select-nvim.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-tmux list-panes -a -F '#{window_index}.#{pane_index}:#{pane_current_command}' | \
-    grep "nvim"| awk -F"[.:]" '{print "select-window -t" $1 "\; select-pane -t" $2}' | \
+nvim=$(tmux list-panes -a -F '#{window_index}.#{pane_index}:#{pane_current_command}' | \
+    grep "nvim")
+if [ "$nvim" != "" ] ; then
+    echo "$nvim" | awk -F"[.:]" '{print "select-window -t" $1 "\\; select-pane -t" $2}' | \
     xargs tmux
+else
+    tmux display "Nvim pane not found"
+fi
 
