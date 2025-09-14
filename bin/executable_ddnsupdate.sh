cb0768122d485b38b7a4bae37266239da704554f
Raghu Rajagopalan <raghu.rajagopalan@rockwellautomation.com>
2025-09-14 11:35:23 +0530

    scripts: remove ddns script

--- bin/executable_ddnsupdate.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-external_ip=$(wget -qO- 'http://myip.dnsdynamic.org/')
-wget  -qO- --no-check-certificate "https://www.dnsdynamic.org/api/?hostname=homepc.ssh01.com&myip=$external_ip"  --user "raghu.rajagopalan@gmail.com" --password view2kil1!
-
-
