9bed1789b1f0bb93069134863559ae8f50a5e3fd
Raghu Rajagopalan <raghu.rajagopalan@rockwellautomation.com>
2025-09-14 22:31:03 +0530

    cf-ddns: update script

--- bin/executable_cf-ddns.sh
+++ bin/executable_cf-ddns.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
-source /home/raghu/.local/cf.secrets
+source ~/.local/cf-ddns.secrets
 ipv6=$(ip -6 a |grep inet6|grep -i global| grep -Pio '2[0-9a-f:]+'| head -n1)
+existingName=${1:-home}
+newName=${2:-$existingName}
+existingDnsName="$existingName.rraghur.in"
+dnsName="$newName.rraghur.in"
+# echo $dnsName
 payload=$(cat <<EOF
-{"type":"AAAA","name":"wg","content":"$ipv6","ttl":1,"priority":10,"proxied":false}
+{"type":"AAAA","name":"$newName","content":"$ipv6","ttl":1,"priority":10,"proxied":false}
 EOF
 )
+read CF_RECID currentIpAddr <<< $(curl -s "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/dns_records?name=$existingDnsName" \
+    -H "Authorization: Bearer $CF_KEY" \
+    -H "Content-Type: application/json" | jq -r '.result[0] | [.id,.content]|@tsv')
 
-# read ipv6 from file; falling back to API if needed
-current=$(cat /tmp/ipv6 || curl -s "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/dns_records/$CF_RECID" \
-     -H "X-Auth-Email: $CF_USER" \
-     -H "X-Auth-Key: $CF_KEY" \
-     -H "Content-Type: application/json"\
-     | grep -Pio '"content":".*?"' \
-     | grep -Pio '2[0-9a-f:]+'\
-     | tee /tmp/ipv6)
+if [ -z $CF_RECID ]; then
+    echo "Failed to locate existing DNS record with name=$existingDnsName"
+    exit 2;
+fi
+
 # logger -t CFDDNS "CF: $current; Machine: $ipv6"
-
-if [ x$current != x$ipv6 ]; then
-         curl -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/dns_records/$CF_RECID" \
-             -H "X-Auth-Email: $CF_USER" \
-             -H "X-Auth-Key: $CF_KEY" \
+if [[ x$currentIpAddr != x$ipv6 || x$existingName != x$newName ]]; then
+    res=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$CF_ZONE/dns_records/$CF_RECID" \
+             -H "Authorization: Bearer $CF_KEY" \
              -H "Content-Type: application/json" \
-             --data "$payload"
-        logger -t CFDDNS "Updated cloudflare IP: $ipv6 - status $?"
+             --data "$payload")
+
+    responseStatus=$(echo $res | jq ".success")
+    if [ x$responseStatus == x"true" ]; then
+        logger -t CFDDNS "Updated cloudflare DDNS: $dnsName -> $ipv6: $responseStatus"
+    else
+        logger -t CFDDNS "Failed to update DDNS record for $dnsName. Error $res"
+        exit 1
+    fi
 else
-        logger -t CFDDNS "Skipping CF update - IP $ipv6 is current"
+    logger -t CFDDNS "Skipping CF update - IP $dnsName -> $ipv6 is current"
 fi
 
